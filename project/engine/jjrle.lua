42b4befc885987c984608e0c441424f1a5bfd78b
Hot Noggin Studios <143744752+HotNoggin@users.noreply.github.com>
2025-03-18 21:15:30 -0500

    Create jjrle.lua

---
+++ project/engine/jjrle.lua
@@ -0,0 +1,19 @@
+local jjrle = {}
+
+
+jjrle.runcodes = {".", }
+
+
+function jjrle.pack(raw)
+end
+
+
+function jjrle.unpack(packed)
+    local raw = ""
+    for i = 1, #packed do
+        
+    end
+end
+
+
+return jjrle
